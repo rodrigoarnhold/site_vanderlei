3fb159f8631541bf37faf93fb50071d94243b113
Rodrigo_Arnhold <rodrigoarnhold2@gmail.com>
2020-04-16 14:52:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+(function () {
+    "use strict"
+
+    var express = require('express');
+    var bodyParser = require('body-parser');
+
+    var server = express();
+
+    server.use(bodyParser.json());
+	server.use(bodyParser.urlencoded({ extended: true }));
+
+    server.all('*', function (req, res, next) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+		res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
+		res.header('Cache-Control', 'no-cache');
+		next();
+    });
+    
+    server.use(express.static('public'));
+    server.listen("80");
+})();
